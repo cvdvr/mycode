66c0c041b1a15e2d095c1afe9d1ccd931df0b1fd
cvdvr <adrian.mccown@gmail.com>
2023-02-14 18:11:19 +0000

    learning about dictionaries

---
+++ bugginout.py
@@ -0,0 +1,20 @@
+#1/usr/bin/python3
+"""Just a lil' warmup!""
+
+name= input(What is your name?\n>)
+num= input("Pick a number between 1 and 3\n>")
+num= num - 1
+
+adj_list= ["stupendous","splendiferous","magnificent"]
+adj= adj_list[num]
+
+# these prints are for debugging purposes, you may delete them when you're ready
+#print(name)
+#print(adj)
+
+# FINAL PART OF CHALLENGE:
+# USE THE name AND adj VARIABLES TO PRINT THE FOLLOWING:
+print("What is your name?")
+print("Pick a number between 1 and 3")
+print( "Hello <name>! Have a <adj> Valentine's Day!")
+
