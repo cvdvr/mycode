1307f4891c3b1b7ce92308a7ec3aca48d3bffd7b
cvdvr <adrian.mccown@gmail.com>
2023-02-14 21:05:27 +0000

    Learning about conditional if

---
+++ iftest/condition01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#create the string hostname
+hostname="MTG"
+
+#test logic with the "if" statement
+#what to do if it is true
+if hostname=="MTG":
+    print("The hostname was found to be MTG")
+
---
+++ marvelscript.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Challenge 26
+#characters dictionary
+marvelchars= {
+"Starlord":
+  {"real name": "peter quill",
+  "powers": "dance moves",
+  "archenemy": "Thanos"},
+
+"Mystique":
+  {"real name": "raven darkholme",
+  "powers": "shape shifter",
+  "archenemy": "Professor X"},
+
+"Hulk":
+  {"real name": "bruce banner",
+  "powers": "super strength",
+  "archenemy": "adrenaline"}
+             }
+print ("Who do you want to know about Starlord, Mystique, or Hulk?")
